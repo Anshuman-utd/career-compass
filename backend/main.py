821f2afdb35112837231dff0f1f6f0ae20134b45
Anshuman-utd <anshumanmehta11@gmail.com>
2026-01-23 00:58:39 +0530

    frontend deployed

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Career Compass API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-    ],
+    "https://career-compass-blush.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
