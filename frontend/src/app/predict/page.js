16c3bb2a70e83d069a8a9fe944a2a8328ca76880
Anshuman-utd <anshumanmehta11@gmail.com>
2026-01-23 00:50:17 +0530

    backend deployed

--- frontend/src/app/predict/page.js
+++ frontend/src/app/predict/page.js
@@ -24,7 +24,7 @@ export default function Predict() {
         setLoading(true);
 
         try {
-            const res = await fetch("http://127.0.0.1:8000/explain", {
+            const res = await fetch("https://career-compass-uphx.onrender.com/explain", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ ...form, internship: Number(form.internship) }),
